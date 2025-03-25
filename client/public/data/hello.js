848702a10fd31a8b6d44e7311fb30ea8e854488f
mebel <ebel@edologic.de>
2025-03-25 20:29:57 +0100

    add hello.js

---
+++ client/public/data/hello.js
@@ -0,0 +1,8 @@
+let slideGroup = [
+    {
+        "title": "Hello World",
+        "titleStyle": "{\"text-align\":\"center\",\"font-weight\":\"bold\"}",
+        "content": "Thx for having me",
+        "contentStyle": "{\"text-align\":\"left\"}",
+    }
+];
